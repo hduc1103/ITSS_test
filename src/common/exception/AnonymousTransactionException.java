b5a44c9c4b68fab7a9276aa910dcc6f2d6853e8d
Mai Hoàng Đức <maihoangduc1103.ptts@gmail.com>
2024-06-24 17:31:55 +0700

    database to use

--- src/common/exception/AnonymousTransactionException.java
+++
@@ -1,7 +0,0 @@
-package common.exception;
-
-public class AnonymousTransactionException extends PaymentException {
-    public AnonymousTransactionException() {
-        super("ERROR: Giao dịch bị nghi ngờ gian lận");
-    }
-}
